565783e10ddc224841caf41968e41b3a2958e3fe
AberQ <egor.master2017@gmail.com>
2025-03-08 18:02:33 +0500

    Добавил базовое отображение

--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Note
 
-# Create your views here.
+def note_list(request):
+    notes = Note.objects.all()
+    return render(request, "note_list.html", {"notes": notes})
