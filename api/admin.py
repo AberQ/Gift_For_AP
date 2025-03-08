112427ee4a245a6e192e109f7a7d58524119bcb3
AberQ <egor.master2017@gmail.com>
2025-03-08 17:57:15 +0500

    Добавил админку

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Note
 
-# Register your models here.
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ("id", "text", "is_read", "is_secret")
+    list_filter = ("is_read", "is_secret")
+    search_fields = ("text",)
+    ordering = ("id",)
